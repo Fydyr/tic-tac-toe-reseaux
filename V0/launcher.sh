2ff115b13341443f6f126911286f4f69e123a9ed
kinator <behani.julien@gmx.fr>
2025-01-07 15:50:42 +0000

    fix sh

--- V0/launcher.sh
+++ V0/launcher.sh
@@ -5,5 +5,3 @@ gcc -c T3N_server_V0.c -Wall -I./include
 gcc -o server T3N_server_V0.o socket_management.o -Wall -I./include
 gcc -c T3N_client_V0.c -Wall -I./include  
 gcc -o client T3N_client_V0.o socket_management.o -Wall -I./include
-gnome-terminal -- bash -c "./server"
-gnome-terminal -- bash -c "./client"
