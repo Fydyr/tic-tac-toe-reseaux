e3aa92c9b52efb58854cba61622f7adc9cd9e28b
Enzo <fournierenzo62@gmail.com>
2025-01-07 10:18:33 +0100

    début code

---
+++ V0/T3N_server_V0.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h> /* pour exit */
+#include <unistd.h> /* pour read, write, close, sleep */
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h> /* pour memset */
+#include <netinet/in.h> /* pour struct sockaddr_in */
+#include <arpa/inet.h> /* pour htons et inet_aton */
+
+#define PORT 5000 //(ports >= 5000 réservés pour usage explicite)
+
+int main(int argc, char *argv[]){
+    
+}
