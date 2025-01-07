b492937fd5973a63b9e0d2cbcd283eaeb9977955
kinator <behani.julien@gmx.fr>
2025-01-07 15:20:45 +0000

    test sh

--- V0/launcher.sh
+++ V0/launcher.sh
@@ -5,6 +5,5 @@ gcc -c T3N_server_V0.c -Wall -I./include
 gcc -o server T3N_server_V0.o socket_management.o -Wall -I./include
 gcc -c T3N_client_V0.c -Wall -I./include  
 gcc -o client T3N_client_V0.o socket_management.o -Wall -I./include
-gnome-terminal -- bash -c "ls; exec bash"
-gnome-terminal -- bash -c "./server; exec bash"
-gnome-terminal -- bash -c "./client; exec bash"
+gnome-terminal -- bash -c "./server"
+gnome-terminal -- bash -c "./client"
