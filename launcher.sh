e536ad31a658f622f5c29b8cd9c3ef83d919467b
Enzo <fournierenzo62@gmail.com>
2025-01-08 13:19:38 +0100

    mise à jour launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 gcc -c include/socket_management.c -Wall -I./include 
-gcc -c T3N_server_V0.c -Wall -I./include  
-gcc -o server T3N_server_V0.o socket_management.o -Wall -I./include
-gcc -c T3N_client_V0.c -Wall -I./include  
-gcc -o client T3N_client_V0.o socket_management.o -Wall -I./include
+gcc -c server.c -Wall -I./include  
+gcc -o server server.o socket_management.o -Wall -I./include
+gcc -c client.c -Wall -I./include  
+gcc -o client client.o socket_management.o -Wall -I./include
