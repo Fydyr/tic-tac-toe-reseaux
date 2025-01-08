bd1f183c4c7a54c3115b86d6eae63bdaa9ab5d84
Zwartkat <zwartkat2@gmail.com>
2025-01-08 13:08:01 +0100

    Clean branch

---
+++ launcher.bat
@@ -0,0 +1,29 @@
+@echo off
+
+echo Compilation des fichiers source dans WSL...
+
+wsl gcc -c include/socket_management.c -Wall -I./include
+
+echo Compilation Socket Management : Fait
+
+wsl gcc -c server.c -Wall -I./include
+wsl gcc -o server server.o socket_management.o -Wall -I./include
+wt -w 0 nt wsl ./server
+
+echo Compilation et lancement serveur : Fait
+
+wsl gcc -c client.c -Wall -I./include
+wsl gcc -o client client.o socket_management.o -Wall -I./include
+wt -w 0 nt wsl ./client 127.0.0.1 5000
+
+echo Compilation et lancement client : Fait
+
+pause
+
+del server
+del client
+del socket_management.o
+del client.o
+del server.o
+
+exit
